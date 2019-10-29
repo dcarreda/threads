586ba292171b73b6ee0a15f6fb94643116292590
dcarreda <diogo.arreda@gmail.com>
2019-10-29 15:45:56 +0000

    multiple threads

--- src/com/webder/threads/Main.java
+++ src/com/webder/threads/Main.java
@@ -6,22 +6,55 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Main thread");
 		MyThread th = new MyThread("child 1");		
-		
-		Thread newThread = new Thread(th);
-		newThread.start();
-		System.out.println("Main thread 2");
+		MyThread th2 = new MyThread("Child 2");
+		MyThread th3 = new MyThread("child 3");		
+		MyThread th4 = new MyThread("Child 4");
+		//Thread newThread = new Thread(th);
 
+	/*	do {
+			System.out.println(".");
+			try {
+				Thread.sleep(100);
+
+			} catch (InterruptedException e) {
+				System.out.println("Main thread interrupted");
+				e.printStackTrace();
+			}
+			
+			//If you want to wait for a thread to finish you use .join()
+			//on the contrary, use .isAlive() to confirm that the thread is still running
+		} while (th.thread.isAlive() || th2.thread.isAlive() || th3.thread.isAlive());
+	*/
+		try {
+			th.thread.join(); //Wait until specified thread ends
+			System.out.println("Child n1 joined");
+			
+			th2.thread.join();
+			System.out.println("Child n2 joined");
+			
+			th3.thread.join();
+			System.out.println("Child n3 joined");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
 
 class MyThread implements Runnable{
+	
+	Thread thread; 
 
 	String myThread;
 	
-
+	// Constructor
 	public MyThread(String myThread) {
 		this.myThread = myThread;
+
+		thread = new Thread(this, myThread);
+		thread.start();
+		
 	}
 
 	
